73974128633157d858011771ab521622770f5b3e
anirbandas10 <anirbandas1127@gmail.com>
2021-09-29 15:17:16 +0530

    Updated

---
+++ JSSampleCode003.js
@@ -0,0 +1,30 @@
+// program to perform function overloading
+
+function sum() {  
+    
+    // if no argument        
+    if (arguments.length == 0) {  
+        console.log('You have not passed any argument');  
+    }
+
+    // if only one argument 
+    else if (arguments.length == 1) {  
+        console.log('Pass at least two arguments');  
+    }
+
+    // multiple arguments
+    else {
+        let result = 0;
+        let length = arguments.length;
+    
+        for (i = 0; i < length; i++) {  
+            result = result + arguments[i];  
+        }  
+        console.log(result); 
+    }  
+}
+
+sum();
+sum(5); 
+sum(5, 9);    
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9); 
